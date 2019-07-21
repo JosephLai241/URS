b925cc62dfad9bbee0a7a5e392c97c1b66be0588
Joseph Lai <48771973+JosephLai241@users.noreply.github.com>
2019-07-20 23:56:44 -0400

    Universal Reddit Scraper 2.0 with CLI Support

---
+++ scraper.py
@@ -0,0 +1,452 @@
+#!/usr/bin/python3.6
+"""
+Created on Sat Jul 20 23:45:06 2019
+
+Reddit scraper using the Reddit API (PRAW)
+
+@author: Joseph Lai
+"""
+import argparse
+import sys
+import praw
+from prawcore import NotFound
+import csv
+import datetime as dt
+
+### Get current date
+date = dt.datetime.now().strftime("%m-%d-%Y")
+
+### Reddit API Credentials
+c_id = "14_CHAR_HERE"               # Personal Use Script (14 char)
+c_secret = "27_CHAR_HERE"           # Secret key (27 char)
+u_a = "APP_NAME_HERE"               # App name
+usrnm = "REDDIT_USERNAME_HERE"      # Reddit username
+passwd = "REDDIT_PASSWORD_HERE"     # Reddit login password
+
+### Subreddit categories
+categories = ["Hot","New","Controversial","Top","Rising","Search"]
+short_cat = [cat[0] for cat in categories]
+
+### Check if subreddit exists
+def existence(reddit,sub_list):
+    found = []
+    not_found = []
+
+    for sub in sub_list:
+        try:
+            reddit.subreddits.search_by_name(sub, exact = True)
+            found.append(sub)
+        except NotFound:
+            not_found.append(sub)
+
+    return found,not_found
+
+### Print Reddit scraper title
+def title():
+    print("""==============================================================
+    Reddit Scraper - Scrape Any Subreddit Of Your Choosing
+==============================================================
+    *Scraper captures posts from all time on the subreddit*""")
+
+#-------------------------------------------------------------------------------
+#                       Command-line Interface functions
+#-------------------------------------------------------------------------------
+### Get args
+def parse_args():
+    parser = argparse.ArgumentParser(usage = "argparse_test.py [-h] [-b] [-s SUBREDDIT [H|N|C|T|R|S] RESULTS_OR_KEYWORDS]", \
+                                    formatter_class = argparse.RawDescriptionHelpFormatter, \
+                                    description = "Universal Reddit Scraper - Scrape any subreddit of your choosing", \
+                                    epilog = """\
+subreddit categories:
+   H,h     selecting Hot category
+   N,n     selecting New category
+   C,c     selecting Controversial category
+   T,t     selecting Top category
+   R,r     selecting Rising category
+   S,s     selecting Search category
+
+EXAMPLES
+
+    Get the first 10 posts in r/all in the Hot posts category:
+
+        $ python cli_scraper.py -s all H 10
+
+    Search for "United States of America" in r/worldnews:
+
+        $ python cli_scraper.py -s worldnews S "United States of America"
+
+    Like the non-CLI scraper, you can choose to scrape multiple subreddits at a time:
+
+        $ python cli_scraper.py -s askreddit C 10 -s dankmemes H 15 -s worldnews S "United States of America"
+
+""")
+
+    ### Parser scraper and results returned flag[H,h]
+    parser.add_argument("-s","--sub", action="append", nargs=3, metavar="", help="specify subreddit to scrape")
+    parser.add_argument("-b","--basic", action="store_true",help="initialize non-CLI Reddit scraper")
+
+    ### Print help message if no arguments are present
+    if len(sys.argv[1:]) == 0:
+        parser.print_help()
+        parser.exit()
+
+    args = parser.parse_args()
+    return parser,args
+
+### Create sub_list if args.basic is False
+def create_sub_list(parser,args):
+    sub_list = [sub[0] for sub in args.sub]
+    return sub_list
+
+### Check args and catching errors
+def check_args(parser,args):
+    sub_counter = 1
+    for subs in args.sub:
+        len_counter = 0
+        try:
+            if subs[1].isdigit() or len(subs[1]) > 1:
+                raise ValueError
+            for char in short_cat:
+                if str(subs[1]).upper() == char:
+                    if str(subs[1]).upper() != "S" and subs[2].isalpha():
+                        raise ValueError
+                    else:
+                        sub_counter += 1
+                    break
+                elif len_counter == len(short_cat) - 1:
+                    raise ValueError
+                else:
+                    len_counter += 1
+        except ValueError:
+            print("ERROR IN FLAG %s.\n" % sub_counter)
+            parser.print_help()
+            parser.exit()
+            break
+
+### Check if the subreddits exist and confirm or reject settings
+def confirm_subs(reddit,sub_list):
+    print("\nChecking if subreddit(s) exist...")
+    found,not_found = existence(reddit,sub_list)
+    if not_found:
+        print("\nThe following subreddits were not found and will be skipped:")
+        print("-"*60)
+        print(*not_found, sep = "\n")
+
+    subs = [sub for sub in found]
+    return subs
+
+### Get CLI scraping settings
+def get_cli_settings(args,master):
+    for sub_n,values in master.items():
+        for sub in args.sub:
+            settings = [sub[1],sub[2]]
+            if sub_n == sub[0]:
+                master[sub_n].append(settings)
+#-------------------------------------------------------------------------------
+
+### Select subreddit(s) to scrape
+def get_subreddits(reddit):
+    title()
+    while True:
+        try:
+            search_for = str(input("""
+Enter subreddit or a list of subreddits (separated by a space) to scrape:
+
+"""))
+            if not search_for:
+                raise ValueError
+
+            print("\nChecking if subreddit(s) exist...")
+            search_for = " ".join(search_for.split())
+            sub_list = [subreddit for subreddit in search_for.split(" ")]
+            found,not_found = existence(reddit,sub_list)
+            if found:
+                print("\nThe following subreddits were found and will be scraped:")
+                print("-"*56)
+                print(*found, sep = "\n")
+            if not_found:
+                print("\nThe following subreddits were not found and will be skipped:")
+                print("-"*60)
+                print(*not_found, sep = "\n")
+
+            while True:
+                try:
+                    confirm = input("\nConfirm selection? [Y/N] ").strip()
+                    if confirm.lower() == "y":
+                        subs = [sub for sub in found]
+                        return subs
+                    elif confirm.lower() == "n":
+                        break
+                    elif confirm.isdigit() or len(confirm) > 1:
+                        raise ValueError
+                    else:
+                        raise ValueError
+
+                except ValueError:
+                    print("Not an option! Try again.")
+
+        except ValueError:
+            print("No subreddits were specified! Try again.")
+        except:
+            print("Error logging in. Check to see if you have provided correct credentials.")
+            pass
+
+### Make master dictionary from subreddit list
+def create_dict(subs):
+    master = dict((sub,[]) for sub in subs)
+    return master
+
+### Select post category and the number of results returned from each subreddit to be scraped
+def get_settings(subs,master):
+    for sub in subs:
+        while True:
+            try:
+                cat_i = int(input(("""
+  Select a category to display for r/%s
+  -------------------
+    0: Hot
+    1: New
+    2: Controversial
+    3: Top
+    4: Rising
+    5: Search
+  -------------------
+           """) % sub))
+
+                if cat_i == 5:
+                    print("\nSelected search option")
+
+                    while True:
+                        try:
+                            search_for = str(input("\nWhat would you like to search for in r/%s? " % sub)).strip()
+
+                            if not search_for:
+                                raise ValueError
+                            else:
+                                for sub_n,values in master.items():
+                                    if sub_n == sub:
+                                        settings = [cat_i,search_for]
+                                        master[sub].append(settings)
+                                break
+
+                        except ValueError:
+                            print("Not an option! Try again.")
+
+                else:
+                    print("\nSelected post category: %s" % categories[cat_i])
+
+                    while True:
+                        try:
+                            submissions = input("\nHow many results do you want to capture from r/%s? " % sub).strip()
+                            if submissions.isalpha() or not submissions:
+                                raise ValueError
+                            else:
+                                for sub_n,values in master.items():
+                                    if sub_n == sub:
+                                        settings = [cat_i,int(submissions)]
+                                        master[sub].append(settings)
+                                break
+
+                        except ValueError:
+                            print("Not an option! Try again.")
+
+                break
+
+            except IndexError:
+                print("Not an option! Try again.")
+            except ValueError:
+                print("Not an option! Try again.")
+
+### Print scraping details for each subreddit
+def print_settings(master,args):
+    print("\n------------------Current settings for each subreddit-------------------")
+    print("\n{:<25}{:<17}{:<30}".format("Subreddit","Category","Number of results / Keyword(s)"))
+    print("-"*72)
+    for sub,settings in master.items():
+        for each in settings:
+            if args.basic == False:
+                cat_i = short_cat.index(each[0].upper())
+                specific = each[1]
+            else:
+                cat_i = each[0]
+                specific = each[1]
+            print("\n{:<25}{:<17}{:<30}".format(sub,categories[cat_i],specific))
+
+    while True:
+        try:
+            confirm = input("\nConfirm options? [Y/N] ").strip()
+            if confirm.lower() == "y":
+                return confirm
+            elif confirm.lower() == "n":
+                break
+            elif confirm.isdigit() or len(confirm) > 1:
+                raise ValueError
+            else:
+                raise ValueError
+
+        except ValueError:
+            print("Not an option! Try again.")
+
+### Get posts of subreddit. Return the dictionary "collected" when done
+def get_posts(reddit,sub,cat_i,search_for,args):
+    print("\nGetting posts for r/%s..." % sub)
+    subreddit = reddit.subreddit(sub)
+
+    if args.basic == False:
+        if cat_i == short_cat[0]:
+            collected = subreddit.hot(limit = int(search_for))
+        elif cat_i == short_cat[1]:
+            collected = subreddit.new(limit = int(search_for))
+        elif cat_i == short_cat[2]:
+            collected = subreddit.controversial(limit = int(search_for))
+        elif cat_i == short_cat[3]:
+            collected = subreddit.top(limit = int(search_for))
+        elif cat_i == short_cat[4]:
+            collected = subreddit.rising(limit = int(search_for))
+        elif cat_i == short_cat[5]:
+            collected = subreddit.search("%s" % search_for)
+    else:
+        if cat_i == 0:
+            collected = subreddit.hot(limit = search_for)
+        elif cat_i == 1:
+            collected = subreddit.new(limit = search_for)
+        elif cat_i == 2:
+            collected = subreddit.controversial(limit = search_for)
+        elif cat_i == 3:
+            collected = subreddit.top(limit = search_for)
+        elif cat_i == 4:
+            collected = subreddit.rising(limit = search_for)
+        elif cat_i == 5:
+            collected = subreddit.search("%s" % search_for)
+
+    return collected
+
+### Sort collected dictionary
+def sort_posts(collected):
+    print("Sorting posts...")
+    overview = {"Title" : [], \
+                "Score" : [], \
+                "ID" : [], \
+                "URL" : [], \
+                "Comment Count" : [], \
+                "Created" : [], \
+                "Text" : []}
+
+    for post in collected:
+        overview["Title"].append(post.title)
+        overview["Score"].append(post.score)
+        overview["ID"].append(post.id)
+        overview["URL"].append(post.url)
+        overview["Comment Count"].append(post.num_comments)
+        overview["Created"].append(dt.datetime.fromtimestamp(post.created).strftime("%m-%d-%Y %H:%M:%S"))    # Convert UNIX time to readable format
+        overview["Text"].append(post.selftext)
+
+    return overview
+
+### Write overview dictionary to CSV
+def write_csv(sub,cat_i,search_for,overview,args):
+    fname = ""
+    if args.basic == False:
+        if cat_i == short_cat[5]:
+            fname = str(("%s-%s-'%s' %s.csv") % (sub,categories[5],search_for,date))
+        else:
+            fname = str(("%s-%s %s.csv") % (sub,categories[short_cat.index(cat_i)],date))
+    else:
+        if cat_i == 5:
+            fname = str(("%s-%s-'%s' %s.csv") % (sub,categories[cat_i],search_for,date))
+        else:
+            fname = str(("%s-%s %s.csv") % (sub,categories[cat_i],date))
+
+    with open(fname, "w") as results:
+        writer = csv.writer(results, delimiter = ",")
+        writer.writerow(overview.keys())
+        writer.writerows(zip(*overview.values()))
+
+    print("CSV file for r/%s created." % sub)
+
+### Get, sort, then write posts to CSV
+def get_sort_write(reddit,args,master):
+    for sub,settings in master.items():
+        for each in settings:
+            if args.basic == False:
+                cat_i = each[0].upper()
+            else:
+                cat_i = each[0]
+            search_for = each[1]
+
+            collected = get_posts(reddit,sub,cat_i,search_for,args)
+            overview = sort_posts(collected)
+            write_csv(sub,cat_i,search_for,overview,args)
+
+def another():
+    options = ["y","n"]
+    while True:
+        try:
+            repeat = input("\nScrape again? [Y/N] ").strip()
+            if repeat.isdigit() or len(repeat) > 1 or str(repeat) not in options or not repeat:
+                raise ValueError
+            else:
+                return str(repeat)
+                break
+        except ValueError:
+            print("Not an option! Try again.")
+
+def main():
+    ### Login loop
+    while True:
+        try:
+            reddit = praw.Reddit(client_id = c_id, \
+                                 client_secret = c_secret, \
+                             	 user_agent = u_a, \
+                             	 username = usrnm, \
+                                 password = passwd)    # Connect to reddit
+
+            break
+
+        except praw.exceptions.APIException as e:   # Catch Reddit API error. REVIEW PARAMS
+            print("\nThere was a server-side error. Try again.")
+            print(e.error_type)
+            print(e.message)
+            print(e.field)
+
+        except praw.exceptions.ClientException:     # Catch client login error
+            print("\nThere was a client-side error. Try again.")
+
+    ### Parse args and initialize basic or CLI scraper
+    parser,args = parse_args()
+    if args.basic == False:
+        ### CLI scraper
+        title()
+        sub_list = create_sub_list(parser,args)
+        check_args(parser,args)
+        subs = confirm_subs(reddit,sub_list)
+
+        master = create_dict(subs)
+        get_cli_settings(args,master)
+        confirm = print_settings(master,args)
+        if confirm == "y":
+            get_sort_write(reddit,args,master)
+        else:
+            print("Exiting.")
+    else:
+        ### Basic scraper
+        print("\nSelected basic scraper\n")
+        while True:
+            while True:
+                subs = get_subreddits(reddit)
+                master = create_dict(subs)
+                get_settings(subs,master)
+                confirm = print_settings(master,args)
+                if confirm == "y":
+                    break
+                else:
+                    parser.exit()
+            get_sort_write(reddit,args,master)
+
+            repeat = another()
+            if repeat == "n":
+                print("\nExiting.")
+                break
+
+if __name__ == "__main__":
+    main()
