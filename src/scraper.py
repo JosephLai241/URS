c62afcba20361fecc7cd6ea65da7a967e49bef98
Joseph Lai <jlai24142@gmail.com>
2020-06-03 15:14:29 -0400

    Updated file to link tools.py

--- src/scraper.py
+++ src/scraper.py
@@ -7,18 +7,11 @@ Universal Reddit Scraper 3.0 - Reddit scraper using the Reddit API (PRAW)
 @author: Joseph Lai
 """
 from colorama import Style, init
-
 import praw
 
-### Import CLI parser, global variables, scrapers and titles
-from . import cli, global_vars, titles
-from .scraper_functions import (basic_functions, comments_functions,
-                                redditor_functions, subreddit_functions)
-from .scrapers import basic, comments, redditor, subreddit
-
-### Global variables
-options = global_vars.options
-s_t = global_vars.s_t
+### Import argparse and scrapers
+from . import cli, titles
+from .tools import Run
 
 ### Reddit API Credentials
 c_id = "14_CHAR_HERE"               # Personal Use Script (14 char)
@@ -41,19 +34,21 @@ def main():
     parser, args = cli.parse_args()
     cli.check_args(parser, args)
     titles.title()
+
+    run = Run(args, parser, reddit)
     
     if args.sub:
         ### Subreddit scraper
-        subreddit.run_subreddit(args, options, parser, reddit, s_t, subreddit_functions)
+        run.subreddit()
     if args.user:
         ### Redditor scraper
-        redditor.run_redditor(args, s_t, parser, reddit, redditor_functions)
+        run.redditor()
     if args.comments:
         ### Post comments scraper
-        comments.run_comments(args, comments_functions, parser, reddit, s_t)
+        run.comments()
     elif args.basic:
         ### Basic Subreddit scraper
-        basic.run_basic(args, basic_functions, options, parser, reddit, subreddit)
+        run.basic()
 
 if __name__ == "__main__":
     try:
