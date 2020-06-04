797f12debd7ba7da6105a0f6694292d368760358
Joseph Lai <joseph@arch.localdomain>
2020-06-04 04:37:43 -0400

    Files now get stored in a directory. Need to change redditor and comments fname

--- urs/utils/export.py
+++ urs/utils/export.py
@@ -3,6 +3,7 @@
 #===============================================================================
 import csv
 import json
+import os
 from . import global_vars
 
 ### Fix fname if illegal filename characters are present
@@ -15,21 +16,22 @@ def r_fname(args, cat_i, search_for, sub, illegal_chars):
     raw_n = ""
     if args.subreddit:
         if cat_i == global_vars.short_cat[5]:
-            raw_n = str(("r-%s-%s-'%s' %s") % 
-                (sub, global_vars.categories[5], search_for, global_vars.date))
+            raw_n = str(("r-%s-%s-'%s'") % 
+                (sub, global_vars.categories[5], search_for))
             fname = fix(raw_n, illegal_chars)
         else:
-            raw_n = str(("r-%s-%s %s") % 
-                (sub, global_vars.categories[global_vars.short_cat.index(cat_i)], global_vars.date))
+            raw_n = str(("r-%s-%s %s results") % 
+                (sub, global_vars.categories[global_vars.short_cat.index(cat_i)], 
+                    search_for))
             fname = fix(raw_n, illegal_chars)
     elif args.basic:
         if cat_i == 5:
-            raw_n = str(("r-%s-%s-'%s' %s") % 
-                (sub, global_vars.categories[cat_i], search_for, global_vars.date))
+            raw_n = str(("r-%s-%s-'%s'") % 
+                (sub, global_vars.categories[cat_i], search_for))
             fname = fix(raw_n, illegal_chars)
         else:
-            raw_n = str(("r-%s-%s %s") % 
-                (sub, global_vars.categories[cat_i], global_vars.date))
+            raw_n = str(("r-%s-%s %s results") % 
+                (sub, global_vars.categories[cat_i], search_for))
             fname = fix(raw_n, illegal_chars)
 
     return fname
@@ -44,13 +46,28 @@ def c_fname(string, illegal_chars):
     raw_n = str(("c-%s %s") % (string, global_vars.date))
     return fix(raw_n, illegal_chars)
 
+def make_directory():
+    scrapes_dir = "../scrapes"
+    if not os.path.isdir(scrapes_dir):
+        os.mkdir(scrapes_dir)
+    
+    sub_path = "../scrapes/%s" % global_vars.date
+    if not os.path.isdir(sub_path):
+        os.mkdir(sub_path)
+    
+    return sub_path
+
 ### Write overview dictionary to CSV or JSON
 def export(fname, overview, f_type):
+    dir_path = make_directory()
+
     if f_type == global_vars.eo[0]:
-        with open("%s.csv" % fname, "w", encoding = "utf-8") as results:
+        filename = dir_path + "/%s.csv" % fname
+        with open(filename, "w", encoding = "utf-8") as results:
             writer = csv.writer(results, delimiter = ",")
             writer.writerow(overview.keys())
             writer.writerows(zip(*overview.values()))
     elif f_type == global_vars.eo[1]:
-        with open("%s.json" % fname, "w", encoding = "utf-8") as results:
+        filename = dir_path + "/%s.json" % fname
+        with open(filename, "w", encoding = "utf-8") as results:
             json.dump(overview, results, indent = 4)
