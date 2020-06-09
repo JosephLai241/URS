59af75bbf7acd44faffe8f55cbd58fc5458f805c
Joseph Lai <joseph@arch.localdomain>
2020-06-09 03:03:36 -0400

    Updated calls

--- urs/tools.py
+++ urs/tools.py
@@ -2,8 +2,8 @@
 #                           All Scrapers
 #===============================================================================
 from colorama import Fore, init, Style
-from utils import (Basic, cli, comments_functions, global_vars,
-                   Redditor, Subreddit, titles)
+from utils import (Basic, Cli, comments_functions, Global,
+                   Redditor, Subreddit, Titles)
 
 ### Automate sending reset sequences to turn off color changes at the end of 
 ### every print
@@ -16,8 +16,14 @@ class Run():
         self.parser = parser
         self.reddit = reddit
 
-        self.options = global_vars.options
-        self.s_t = global_vars.s_t
+        self.options = Global.options
+        self.s_t = Global.s_t
+
+    ### Run checks for CLI args and PRAW credentials.
+    def check(self):
+        parser, args = parse_args()
+        Validation().validate_user(parser, reddit)
+        check_args(parser, args)
 
     ### Run Subreddit scraper
     def subreddit(self):
@@ -26,15 +32,15 @@ class Run():
     ### Run Redditor scraper
     def redditor(self):
         Redditor.RunRedditor().run(self.args, self.parser, self.reddit)
-        
+
     ### Run comments scraper
     def comments(self):
-        titles.c_title()
+        Titles.Titles().c_title()
 
-        post_list = cli.create_list(self.args, self.s_t, self.s_t[2])
+        post_list = Cli.GetScrapeSettings().create_list(self.args, self.s_t[2])
         posts = comments_functions.list_posts(self.reddit, post_list, self.parser)
         c_master = comments_functions.c_c_dict(posts)
-        cli.get_cli_settings(self.reddit, self.args, c_master, self.s_t, self.s_t[2])
+        Cli.get_cli_settings(self.reddit, self.args, c_master, self.s_t, self.s_t[2])
 
         comments_functions.w_comments(self.reddit, post_list, c_master, self.args)
 
