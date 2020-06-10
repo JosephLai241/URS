c3bbe410ec7f811ae8d830b2f3c4fd3387512ae0
Joseph Lai <joseph@arch.localdomain>
2020-06-10 05:18:12 -0400

    Moved directory creation function to a separate file

---
+++ urs/utils/DirInit.py
@@ -0,0 +1,23 @@
+#===============================================================================
+#                           Initialize Directories
+#===============================================================================
+import os
+
+from . import Global
+
+class InitializeDirectory():
+    """
+    On the first run, create the directory scrapes/. Then make a sub-directory 
+    corresponding with the date in which the user scraped data from Reddit if it 
+    does not exist.
+    """
+    
+    @staticmethod
+    def make_directory():
+        scrapes_dir = "../scrapes"
+        if not os.path.isdir(scrapes_dir):
+            os.mkdir(scrapes_dir)
+        
+        dir_path = "../scrapes/%s" % Global.date
+        if not os.path.isdir(dir_path):
+            os.mkdir(dir_path)
