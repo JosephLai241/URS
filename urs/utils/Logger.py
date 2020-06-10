9717835f110b90f8f7a7ba5642c50c61142ea8c2
Joseph Lai <joseph@arch.localdomain>
2020-06-10 05:21:10 -0400

    Broke loggers into separate classes

--- urs/utils/Logger.py
+++ urs/utils/Logger.py
@@ -4,12 +4,22 @@
 import logging
 import time
 
-from . import Global
+from colorama import Fore, init, Style
 
-class LogRuntime():
+from . import DirInit, Global
+
+### Automate sending reset sequences to turn off color changes at the end of 
+### every print.
+init(autoreset = True)
+
+class LogMain():
     """
-    Decorator for logging URS runtime.
+    Decorator for logging URS runtime. Also handles KeyboardInterrupt and adds the
+    event to the log if applicable.
     """
+
+    ### Makes directory in which the log and scraped files will be stored.
+    DirInit.InitializeDirectory.make_directory()
 
     ### Set directory path and log format.
     DIR_PATH = "../scrapes/%s" % Global.date
@@ -19,15 +29,147 @@ class LogRuntime():
     logging.basicConfig(filename = DIR_PATH + "/scrapes.log", 
                         format = LOG_FORMAT,
                         level = logging.INFO)
-
-    ### Wrapper for logging the amount of time it took to execute main().
+    
+    ### Wrapper for logging the amount of time it took to execute main(). Handle
+    ### KeyboardInterrupt if user cancels scraping.
     @staticmethod
-    def main_timer(function):
+    def master_timer(function):
         def wrapper(*args):
-            start = time.time()
-            function(*args)
+            logging.info("Initializing URS...")
+            logging.info("")
 
-            runtime = "Completed scraping in %.2f seconds" % (time.time() - start)
+            start = time.time()
+            try:
+                function(*args)
+            except KeyboardInterrupt:
+                print(Style.BRIGHT + Fore.RED + "\n\nURS ABORTED BY USER.\n")
+                logging.critical("")
+                logging.critical("URS ABORTED BY USER.\n")
+                quit()
+
+            runtime = "URS completed scrapes in %.2f seconds.\n" % \
+                (time.time() - start)
+            
             logging.info((runtime))
+
+        return wrapper
+
+class LogScraper():
+    """
+    Decorator for logging runtimes and program events. 
+    """
+
+    ### Get scraper type for logging.
+    @staticmethod
+    def get_args_switch(args, scraper):
+        scrapers = {
+            Global.s_t[0]: [arg_set for arg_set in args.subreddit] if args.subreddit \
+                else None,
+            Global.s_t[1]: [arg_set for arg_set in args.redditor] if args.redditor \
+                else None,
+            Global.s_t[2]: [arg_set for arg_set in args.comments] if args.comments \
+                else None
+        }
+
+        return scrapers.get(scraper)
+
+    ### Replace the second tuple item with the full category name if Subreddit
+    ### scraper is selected.
+    @staticmethod
+    def subreddit_tuple(each_arg):
+        args_list = list(each_arg)
+        args_list[1] = Global.categories[Global.short_cat.index(each_arg[1].upper())] 
+        return tuple(args_list)
+
+    ### Get the full Subreddit category name for each Subreddit tuple.
+    @staticmethod
+    def format_subreddit_settings(scraper, scraper_args):
+        args = []
+        for each_arg in scraper_args:
+            settings = LogScraper.subreddit_tuple(each_arg) if scraper == Global.s_t[0] \
+                else tuple(each_arg)
+
+            args.append(settings)
+
+        return args
+
+    ### Format Subreddit log differently if user searched for keywords.
+    @staticmethod
+    def format_subreddit_log(each_arg):
+        return "Scraping r/%s for %s %s results..." % each_arg \
+            if each_arg[1] != Global.categories[5] else \
+                "Searching and scraping r/%s for posts containing '%s'..." % \
+                    (each_arg[0], each_arg[2])
+
+    ### Format Redditor log depending on number of results scraped.
+    @staticmethod
+    def format_redditor_log(each_arg):
+        return "Scraping %s results for u/%s..." % (each_arg[1], each_arg[0]) \
+            if int(each_arg[1]) > 1 else \
+                "Scraping %s result for u/%s..." % (each_arg[1], each_arg[0])
+
+    ### Format comments log depending on raw or structured export.
+    @staticmethod
+    def format_comments_log(each_arg):
+        return "Processing %s comments and including second and third-level replies from Reddit post %s" % \
+            (each_arg[1], each_arg[0]) if int(each_arg[1]) > 0 else \
+                "Processing all comments in raw format from Reddit post %s" % each_arg[0]
+
+    ### Format string for log file depending on what was scraped, then log the 
+    ### string.
+    @staticmethod
+    def format_scraper_log(scraper, args_list):
+        args = LogScraper.format_subreddit_settings(scraper, args_list)
+        for each_arg in args:
+            formats = {
+                Global.s_t[0]: LogScraper.format_subreddit_log(each_arg) \
+                    if scraper == Global.s_t[0] else None,
+                Global.s_t[1]: LogScraper.format_redditor_log(each_arg) \
+                    if scraper == Global.s_t[1] else None,
+                Global.s_t[2]: LogScraper.format_comments_log(each_arg) \
+                    if scraper == Global.s_t[2] else None
+            }
+
+            logging.info(formats.get(scraper))
+
+    ### Wrapper for logging the amount of time it took to execute a scraper.
+    @staticmethod
+    def scraper_timer(scraper):
+        def decorator(function):
+            def wrapper(*args):
+                start = time.time()
+                
+                start_log = "Running %s scraper..." % scraper.capitalize()
+                logging.info((start_log))
+
+                function(*args)
+
+                LogScraper.format_scraper_log(scraper, 
+                    LogScraper.get_args_switch(args[1], scraper))
+
+                runtime = "%s scraper finished in %.2f seconds." %\
+                    (scraper.capitalize(), time.time() - start)
+                logging.info((runtime))
+                logging.info("")
+
+            return wrapper
+        return decorator
+
+class LogExport():
+    """
+    Decorator for logging exporting files.
+    """
+
+    @staticmethod
+    def log_export(function):
+        def wrapper(*args):
+            try:
+                function(*args)
+                export_log = "Exporting scrapes to JSON files." if args[1].json else \
+                    "Exporting scrapes to CSV files."
+                logging.info(export_log)
+                logging.info("")
+            except Exception as e:
+                logging.critical("AN ERROR HAS OCCURED WHILE EXPORTING SCRAPED DATA.")
 
         return wrapper
