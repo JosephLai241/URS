bcfaa9eb2aaf2dfa6c01babd8e172a16900493ce
Joseph Lai <joseph@arch.localdomain>
2020-06-15 03:07:50 -0400

    Getting rid of unnecessary @staticmethod decorators

--- urs/utils/Cli.py
+++ urs/utils/Cli.py
@@ -19,9 +19,9 @@ class Parser():
 
     ### Initialize objects that will be used in class methods.
     def __init__(self):
-        self._usage = "Urs.py [-h] [-r SUBREDDIT [H|N|C|T|R|S] RESULTS_OR_KEYWORDS] [-u USER RESULTS] [-c URL RESULTS] [-b] [-y] [--csv|--json]"
+        self._usage = "$ Urs.py [-h] [-r SUBREDDIT [H|N|C|T|R|S] RESULTS_OR_KEYWORDS] [-u USER RESULTS] [-c URL RESULTS] [-b] [-y] [--csv|--json]"
         self._description = r"""
-Universal Reddit Scraper 3.1 - Scrape Subreddits, submissions, Redditors, or comments from submissions
+Universal Reddit Scraper 3.1 - Scrape Subreddits, Redditors, or submission comments
 
 Author: Joseph Lai
 Contact: urs_project@protonmail.com
@@ -71,8 +71,7 @@ You can also still use URS 1.0 (SUBREDDIT SCRAPING ONLY), but you cannot include
 
     ### Add parser Subreddit, Redditor, comments, basic scraper, and skip 
     ### confirmation flags.
-    @staticmethod
-    def _add_flags(parser):
+    def _add_flags(self, parser):
         urs = parser.add_argument_group("Scraping options")
         urs.add_argument("-r", "--subreddit", action = "append", nargs = 3, 
             metavar = "", help = "specify Subreddit to scrape")
@@ -86,8 +85,7 @@ You can also still use URS 1.0 (SUBREDDIT SCRAPING ONLY), but you cannot include
             help = "skip Subreddit options confirmation and scrape immediately")
 
     ### Add export flags.
-    @staticmethod
-    def _add_export(parser):
+    def _add_export(self, parser):
         expt = parser.add_mutually_exclusive_group(required = True)
         expt.add_argument("--csv", action = "store_true", help = "export to CSV")
         expt.add_argument("--json", action = "store_true", help = "export to JSON")
@@ -98,8 +96,8 @@ You can also still use URS 1.0 (SUBREDDIT SCRAPING ONLY), but you cannot include
             epilog = self._epilog, formatter_class = argparse.RawDescriptionHelpFormatter,
             usage = self._usage)
 
-        Parser._add_flags(parser)
-        Parser._add_export(parser)
+        self._add_flags(parser)
+        self._add_export(parser)
 
         ### Print help message if no arguments are present.
         if len(sys.argv[1:]) == 0:
@@ -119,8 +117,7 @@ class GetScrapeSettings():
         self._s_t = Global.s_t
 
     ### Switch to determine which kind of list to create.
-    @staticmethod
-    def _list_switch(args, index):
+    def _list_switch(self, args, index):
         switch = {
             0: args.subreddit,
             1: args.redditor,
@@ -132,13 +129,12 @@ class GetScrapeSettings():
     ### Create either Subreddit, Redditor, or posts list.
     def create_list(self, args, l_type):
         index = self._s_t.index(l_type)
-        list = [item[0] for item in GetScrapeSettings._list_switch(args, index)]
+        list = [item[0] for item in self._list_switch(args, index)]
 
         return list
 
     ### Get Subreddit settings.
-    @staticmethod
-    def _subreddit_settings(args, master):
+    def _subreddit_settings(self, args, master):
         for sub_n in master:
             for sub in args.subreddit:
                 settings = [sub[1], sub[2]]
@@ -147,19 +143,18 @@ class GetScrapeSettings():
 
     ### Get settings for scraping items that only require two arguments 
     ### (Redditor or comments scrapers).
-    @staticmethod
-    def _two_arg_settings(master, object):
+    def _two_arg_settings(self, master, object):
         for obj in object:
             master[obj[0]] = obj[1]
 
     ### Get CLI scraping settings for Subreddits, Redditors, and post comments.
     def get_settings(self, args, master, reddit, s_type):
         if s_type == self._s_t[0]:
-            GetScrapeSettings._subreddit_settings(args, master)
+            self._subreddit_settings(args, master)
         elif s_type == self._s_t[1]:
-            GetScrapeSettings._two_arg_settings(master, args.redditor)
+            self._two_arg_settings(master, args.redditor)
         elif s_type == self._s_t[2]:
-            GetScrapeSettings._two_arg_settings(master, args.comments)
+            self._two_arg_settings(master, args.comments)
 
 class CheckCli():
     """
@@ -183,16 +178,14 @@ class CheckCli():
                         raise ValueError
 
     ### Check Redditor args
-    @staticmethod
-    def _check_redditor(args):
+    def _check_redditor(self, args):
         for users in args.redditor:
             if users[1].isalpha() or int(users[1]) == 0:
                 raise ValueError
 
     ### Check args for items that only require two arguments (Redditor or 
     ### comments scrapers).
-    @staticmethod
-    def _check_comments(args):
+    def _check_comments(self, args):
         for comments in args.comments:
             if comments[1].isalpha():
                 raise ValueError
@@ -203,6 +196,6 @@ class CheckCli():
         if args.subreddit:
             self._check_subreddit(args)
         if args.redditor:
-            CheckCli._check_redditor(args)
+            self._check_redditor(args)
         if args.comments:
-            CheckCli._check_comments(args)
+            self._check_comments(args)
