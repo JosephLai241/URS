74c62309c0c20070812f422c92821965367e78be
Joseph Lai <joseph@arch.localdomain>
2020-06-15 22:28:43 -0400

    Each function should only have one assert statement

--- tests/test_Export.py
+++ tests/test_Export.py
@@ -2,7 +2,8 @@ from urs.utils import Export, Global
 
 class TestExport():
     """
-    Testing export functions.
+    Testing export functions. Function names are pretty self-explanatory, so I 
+    will not be adding comments above the functions.
     """
 
     def test_fix(self):
@@ -11,12 +12,14 @@ class TestExport():
 
         assert fixed == Export.NameFile()._fix(name)
 
-    def test_r_category(self):
+    def test_r_category_first_switch(self):
         assert Export.NameFile()._r_category(None, 0) == Global.categories[5]
 
+    def test_r_category_second_switch(self):
         for index, category in enumerate(Global.short_cat[:5]):
             assert Export.NameFile()._r_category(category, 1) == Global.categories[index]
 
+    def test_r_category_third_switch(self):
         for i in range(0, len(Global.categories)):
             assert Export.NameFile()._r_category(i, 2) == Global.categories[i]
                 
