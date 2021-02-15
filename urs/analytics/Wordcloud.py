f8211a18d8cf6ca7b9addc7eebc26c0960e1069c
Joseph Lai <jlai24142@gmail.com>
2021-02-14 21:51:33 -0500

    Importing loggers. Split up helper methods into their respective classes

--- urs/analytics/Wordcloud.py
+++ urs/analytics/Wordcloud.py
@@ -19,30 +19,38 @@ from analytics.utils.PrepData import (
 )
 
 from urs.utils.DirInit import InitializeDirectory
+from urs.utils.Global import analytical_tools
+from urs.utils.Logger import LogAnalytics
 from urs.utils.Titles import AnalyticsTitles
 
 ### Automate sending reset sequences to turn off color changes at the end of 
 ### every print.
 init(autoreset = True)
 
-class GenerateWordcloud():
+class SetUpWordcloud():
     """
-    Methods for generating a wordcloud.
+    Methods for setting up the wordcloud.
     """
 
     ### Initialize wordcloud.
-    def _initialize_wordcloud(self, file, scrape_type):
+    def initialize_wordcloud(self, file, scrape_type):
         return WordCloud().generate_from_frequencies(PrepData.prep(file[0], scrape_type))
 
     ### Set wordcloud preferences.
-    def _modify_wordcloud(self, wc):
+    def modify_wordcloud(self, wc):
         plt.imshow(wc, interpolation = "bilinear")
         plt.axis("off")
 
         return plt
 
+class FinalizeWordcloud():
+    """
+    Methods for either saving or displaying the wordcloud.
+    """
+
     ### Show wordcloud.
-    def _show_wordcloud(self, plt):
+    @LogAnalytics.log_show(analytical_tools[2])
+    def show_wordcloud(self, plt):
         display_msg = "\nDisplaying wordcloud..."
         print(Style.BRIGHT + Fore.GREEN + display_msg)
         print(Style.BRIGHT + Fore.GREEN + "-" * (len(display_msg) - 1))
@@ -50,7 +58,8 @@ class GenerateWordcloud():
         plt.show()
 
     ### Save wordcloud.
-    def _save_wordcloud(self, file, wc):
+    @LogAnalytics.log_save(analytical_tools[2])
+    def save_wordcloud(self, file, wc):
         date_dir, filename = GetPath.name_file(file[1], file[0], "wordclouds")
         InitializeDirectory.make_analytics_directory(date_dir, "wordclouds")
         wc.to_file(filename)     
@@ -58,19 +67,27 @@ class GenerateWordcloud():
         confirmation = "\nWordcloud exported to %s" % filename
         print(Style.BRIGHT + Fore.GREEN + confirmation)
         print(Style.BRIGHT + Fore.GREEN + "-" * (len(confirmation) - 1))
-    
+
+        return filename
+
+class GenerateWordcloud():
+    """
+    Methods for generating a wordcloud.
+    """
+
     ### Generate wordcloud.
-    def generate(self, args):
+    @staticmethod
+    @LogAnalytics.generator_timer(analytical_tools[2])
+    def generate(args):
         AnalyticsTitles.wc_title()
 
         for file in args.wordcloud:
             print("\nGenerating wordcloud...\n")
 
             scrape_type = GetPath.get_scrape_type(file[0])
-            wc = self._initialize_wordcloud(file, scrape_type)
-            plt = self._modify_wordcloud(wc)
+            wc = SetUpWordcloud().initialize_wordcloud(file, scrape_type)
+            plt = SetUpWordcloud().modify_wordcloud(wc)
             
-            self._show_wordcloud(plt) \
+            FinalizeWordcloud().show_wordcloud(plt) \
                 if args.nosave \
-                else self._save_wordcloud(file, wc)
-    
+                else FinalizeWordcloud().save_wordcloud(file, wc)
