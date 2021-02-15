1c1742724e1c7160ba71a319280e6ef7b57915d2
Joseph Lai <jlai24142@gmail.com>
2021-02-14 21:32:17 -0500

    Upgrading from relative to absolute imports. Updating switch logic for future scalability. Added new check arg and analytical tools to the switch

--- urs/utils/Tools.py
+++ urs/utils/Tools.py
@@ -1,28 +1,22 @@
 #===============================================================================
 #                   All Scrapers and Validation/Args Checking
 #===============================================================================
-from colorama import (
-    init, 
-    Fore, 
-    Style
-)
+from analytics.Chart import GenerateChart
+from analytics.Frequencies import GenerateFrequencies
+from analytics.Wordcloud import GenerateWordcloud
 
-from praw_scrapers import (
-    Basic,
-    Comments, 
-    Redditor, 
-    Subreddit
-)
-from utils import (
-    Cli, 
-    Global, 
-    Titles, 
-    Validation
-)
+from praw_scrapers.Basic import RunBasic
+from praw_scrapers.Comments import RunComments
+from praw_scrapers.Redditor import RunRedditor
+from praw_scrapers.Subreddit import RunSubreddit
 
-### Automate sending reset sequences to turn off color changes at the end of 
-### every print.
-init(autoreset = True)
+from utils.Cli import (
+    CheckCli,
+    Parser
+)
+from utils.Global import s_t
+from utils.Titles import MainTitle
+from utils.Validation import Validation
 
 class Run():
     """
@@ -32,32 +26,51 @@ class Run():
     ### Initialize objects that will be used in class methods.
     def __init__(self, reddit):
         self._reddit = reddit
-        self._args, self._parser = self._login_and_args()
-
-        self._s_t = Global.s_t
-
+        self._args, self._parser = self._introduce_then_args()
+        
     ### Print title, then run checks for CLI args and PRAW credentials.
-    def _login_and_args(self):
-        Titles.Titles.title()
+    def _introduce_then_args(self):
+        MainTitle.title()
 
-        args, parser = Cli.Parser().parse_args()
-        Validation.Validation.validate_user(parser, self._reddit)
-        Cli.CheckCli().check_args(args, parser)
+        args, parser = Parser().parse_args()
+        CheckCli().check_args(args, parser)
 
         return args, parser
 
     ### Switch for running scrapers.
     def run_urs(self):
-        ### Run Subreddit scraper.
-        if self._args.subreddit:
-            Subreddit.RunSubreddit.run(self._args, self._parser, self._reddit, self._s_t)
-        ### Run Redditor scraper.
-        if self._args.redditor:
-            Redditor.RunRedditor.run(self._args, self._parser, self._reddit)
-        ### Run submission comments scraper.
-        if self._args.comments:
-            Comments.RunComments.run(self._args, self._parser, self._reddit)
-        ### Run basic Subreddit scraper.
-        elif self._args.basic:
-            Basic.RunBasic.run(self._args, self._parser, self._reddit)
+        ### Run rate limit check.
+        if self._args.check:
+            Validation.validate_user(self._parser, self._reddit)
+
+        ### Run PRAW scrapers.
+        elif self._args.subreddit or self._args.redditor or self._args.comments or self._args.basic:
+            ### Validate PRAW credentials, get user rate limit information, and
+            ### finally check for valid Subreddits, Redditors, or submission URLs.
+            Validation.validate_user(self._parser, self._reddit)
+
+            ### Run Subreddit scraper.
+            if self._args.subreddit:
+                RunSubreddit.run(self._args, self._parser, self._reddit, s_t)
+            ### Run Redditor scraper.
+            if self._args.redditor:
+                RunRedditor.run(self._args, self._parser, self._reddit)
+            ### Run submission comments scraper.
+            if self._args.comments:
+                RunComments.run(self._args, self._parser, self._reddit)
+            ### Run basic Subreddit scraper.
+            elif self._args.basic:
+                RunBasic.run(self._args, self._parser, self._reddit)
+        
+        ### Run analytical tools.
+        elif self._args.frequencies or self._args.chart or self._args.wordcloud:
+            ### Run frequencies generator.
+            if self._args.frequencies:
+                GenerateFrequencies().generate(self._args)
+            ### Run chart generator.
+            if self._args.chart:
+                GenerateChart.generate(self._args)
+            ### Run wordcloud generator.
+            if self._args.wordcloud:
+                GenerateWordcloud().generate(self._args)
         
