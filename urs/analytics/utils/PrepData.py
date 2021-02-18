10d9b85e722118eb2226253a40384318c4b7d75a
Joseph Lai <jlai24142@gmail.com>
2021-02-18 16:30:35 -0500

    Moving cleaner methods to its own class

--- urs/analytics/utils/PrepData.py
+++ urs/analytics/utils/PrepData.py
@@ -42,84 +42,9 @@ class Extract():
         with open(str(file), "r", encoding = "utf-8") as raw_data:
             return json.load(raw_data)
 
-class PrepSubreddit():
+class CleanData():
     """
-    Methods for preparing Subreddit data.
-    """
-
-    ### Prepare Subreddit data.
-    @staticmethod
-    def prep_subreddit(data, file):
-        plt_dict = dict()
-
-        for submission in data:
-            PrepData.count_words("title", submission, plt_dict)
-            PrepData.count_words("text", submission, plt_dict)
-
-        return dict(sorted(plt_dict.items(), key = lambda item: item[1], reverse = True))
-
-class PrepRedditor():
-    """
-    Methods for preparing Redditor data.
-    """
-
-    ### Prepare Redditor data.
-    @staticmethod
-    def prep_redditor(data, file):
-        plt_dict = dict()
-
-        for interactions in data["interactions"].values():
-            for obj in interactions:
-                ### Indicates there is valid data in this field.
-                if isinstance(obj, dict):
-                    if obj["type"] == "submission":
-                        PrepData.count_words("title", obj, plt_dict)
-                        PrepData.count_words("body", obj, plt_dict)
-                    elif obj["type"] == "comment":
-                        PrepData.count_words("text", obj, plt_dict)
-                ### Indicates this field is forbidden.
-                elif isinstance(obj, str):
-                    continue
-
-        return dict(sorted(plt_dict.items(), key = lambda item: item[1], reverse = True))
-
-class PrepComments():
-    """
-    Methods for preparing submission comments data.
-    """
-
-    ### Prepare RAW submission comments.
-    @staticmethod
-    def _prep_raw(data, plt_dict):
-        for comment_data in data[0].values():
-            PrepData.count_words("text", comment_data, plt_dict)
-
-    ### A recursive method to prepare structured submission comments.
-    @staticmethod
-    def _prep_structured(data, plt_dict):
-        for comment_object in data:
-            for comment_data in comment_object.values():
-                PrepData.count_words("text", comment_data, plt_dict)
-
-                ### Recursive call if the comment contains the "replies" field and 
-                ### if there are comments within the replies list.
-                if "replies" in comment_data.keys() and comment_data["replies"]:
-                    PrepComments._prep_structured(comment_data["replies"], plt_dict)
-    
-    ### Prepare submission comments data.
-    @staticmethod
-    def prep_comments(data, file):
-        plt_dict = dict()
-
-        PrepComments._prep_raw(data["data"], plt_dict) \
-            if data["scrape_settings"]["n_results"] == "RAW" \
-            else PrepComments._prep_structured(data["data"], plt_dict)
-
-        return dict(sorted(plt_dict.items(), key = lambda item: item[1], reverse = True))
-    
-class PrepData():
-    """
-    Calling all methods for preparing scraped data. 
+    Methods for cleaning words found in "title", "body" or "text" fields.
     """
 
     ### Removing unnecessary characters from words.
@@ -139,7 +64,7 @@ class PrepData():
     def count_words(field, obj, plt_dict):
         words = obj[field].split(" ")
         for word in words:
-            word = PrepData._remove_extras(word)
+            word = CleanData._remove_extras(word)
             if not word:
                 continue
             
@@ -147,6 +72,86 @@ class PrepData():
                 plt_dict[word] = 1
             else:
                 plt_dict[word] += 1
+
+class PrepSubreddit():
+    """
+    Methods for preparing Subreddit data.
+    """
+
+    ### Prepare Subreddit data.
+    @staticmethod
+    def prep_subreddit(data, file):
+        plt_dict = dict()
+
+        for submission in data:
+            CleanData.count_words("title", submission, plt_dict)
+            CleanData.count_words("text", submission, plt_dict)
+
+        return dict(sorted(plt_dict.items(), key = lambda item: item[1], reverse = True))
+
+class PrepRedditor():
+    """
+    Methods for preparing Redditor data.
+    """
+
+    ### Prepare Redditor data.
+    @staticmethod
+    def prep_redditor(data, file):
+        plt_dict = dict()
+
+        for interactions in data["interactions"].values():
+            for obj in interactions:
+                ### Indicates there is valid data in this field.
+                if isinstance(obj, dict):
+                    if obj["type"] == "submission":
+                        CleanData.count_words("title", obj, plt_dict)
+                        CleanData.count_words("body", obj, plt_dict)
+                    elif obj["type"] == "comment":
+                        CleanData.count_words("text", obj, plt_dict)
+                ### Indicates this field is forbidden.
+                elif isinstance(obj, str):
+                    continue
+
+        return dict(sorted(plt_dict.items(), key = lambda item: item[1], reverse = True))
+
+class PrepComments():
+    """
+    Methods for preparing submission comments data.
+    """
+
+    ### Prepare RAW submission comments.
+    @staticmethod
+    def _prep_raw(data, plt_dict):
+        for comment_data in data[0].values():
+            CleanData.count_words("text", comment_data, plt_dict)
+
+    ### A recursive method to prepare structured submission comments.
+    @staticmethod
+    def _prep_structured(data, plt_dict):
+        for comment_object in data:
+            for comment_data in comment_object.values():
+                CleanData.count_words("text", comment_data, plt_dict)
+
+                ### Recursive call if the comment contains the "replies" field and 
+                ### if there are comments within the replies list.
+                if "replies" in comment_data.keys() and comment_data["replies"]:
+                    PrepComments._prep_structured(comment_data["replies"], plt_dict)
+    
+    ### Prepare submission comments data.
+    @staticmethod
+    def prep_comments(data, file):
+        plt_dict = dict()
+
+        PrepComments._prep_raw(data["data"], plt_dict) \
+            if data["scrape_settings"]["n_results"] == "RAW" \
+            else PrepComments._prep_structured(data["data"], plt_dict)
+
+        return dict(sorted(plt_dict.items(), key = lambda item: item[1], reverse = True))
+
+class PrepData():
+    """
+    Calling all methods for preparing scraped data. 
+    """
 
     ### Combine all prep methods into one public method.
     @staticmethod
