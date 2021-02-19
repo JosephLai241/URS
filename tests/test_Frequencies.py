a31040bfa8840b4d804d6bec611ab4097a5a1e17
Joseph Lai <jlai24142@gmail.com>
2021-02-19 01:43:46 -0500

    Adding new tests for analytical tools

---
+++ tests/test_Frequencies.py
@@ -0,0 +1,77 @@
+from urs.analytics import Frequencies
+
+### Function names are pretty self-explanatory, so I will not be adding comments 
+### above the functions.
+
+class TestSortGetDataMethod():
+    """
+    Testing Sort class get_data() method found on line 39 in Frequencies.py.
+    """
+
+    def test_get_data_method(self):
+        pass
+
+class TestSortNameAndCreateDirMethod():
+    """
+    Testing Sort class name_and_create_dir() method found on line 44 in 
+    Frequencies.py.
+    """
+
+    def test_name_and_create_dir_method(self):
+        pass
+
+class TestSortCreateCsvMethod():
+    """
+    Testing Sort class create_csv() method found on line 55 in Frequencies.py.
+    """
+
+    def test_create_csv_method(self):
+        plt_dict = {
+            "test": 1,
+            "testing": 2
+        }
+
+        assert Frequencies.Sort().create_csv(plt_dict) == {
+            "words": ["test", "testing"],
+            "frequencies": [1, 2]
+        }
+    
+class TestSortCreateJsonMethod():
+    """
+    Testing Sort class create_json() method found on line 68 in Frequencies.py.
+    """
+
+    def test_create_json_method(self):
+        file = ["test", "something"]
+        plt_dict = {
+            "test": 1,
+            "testing": 2
+        }
+
+        assert Frequencies.Sort().create_json(file, plt_dict) == {
+            "raw_file": "test",
+            "data": [
+                {
+                    "test": 1,
+                    "testing": 2
+                }
+            ]
+        }
+
+class TestExportFrequenciesExportMethod():
+    """
+    Testing ExportFrequencies class export() method found on line 82 in 
+    Frequencies.py.
+    """
+
+    def test_export_method(self):
+        pass
+
+
+class TestPrintConfirmConfirmMethod():
+    """
+    Testing PrintConfirm class confirm() method found on line 93 in Frequencies.py.
+    """
+
+    def test_confirm_method(self):
+        pass
---
+++ tests/test_Wordcloud.py
@@ -0,0 +1,40 @@
+from urs.analytics import Wordcloud
+
+### Function names are pretty self-explanatory, so I will not be adding comments 
+### above the functions.
+
+class TestSetUpWordcloudInitializeWordcloudMethod():
+    """
+    Testing SetUpWordcloud class initialize_wordcloud() method found on line 41
+    in Wordcloud.py.
+    """
+
+    def test_initialize_wordcloud_method(self):
+        pass
+
+class TestSetUpWordcloudModifyWordcloudMethod():
+    """
+    Testing SetUpWordcloud class modify_wordcloud() method found on line 41
+    in Wordcloud.py.
+    """
+
+    def test_modify_wordcloud_method(self):
+        pass
+
+class TestFinalizeWordcloudShowWordcloudMethod():
+    """
+    Testing FinalizeWordcloud class show_wordcloud() method found on line 63 in
+    Wordcloud.py.
+    """
+
+    def test_show_wordcloud_method(self):
+        pass
+
+class TestFinalizeWordcloudSaveWordcloudMethod():
+    """
+    Testing FinalizeWordcloud class save_wordcloud() method found on line 72 in
+    Wordcloud.py.
+    """
+
+    def test_save_wordcloud_method(self):
+        pass
