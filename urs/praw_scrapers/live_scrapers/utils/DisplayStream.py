5dec281797438d810e02fa77b188961ce76757e5
Joseph Lai <jlai24142@gmail.com>
2021-05-06 20:18:04 -0400

    Setting up PrettyTable for stream objects

--- urs/praw_scrapers/live_scrapers/utils/DisplayStream.py
+++ urs/praw_scrapers/live_scrapers/utils/DisplayStream.py
@@ -9,11 +9,40 @@ from colorama import (
     Fore,
     Style
 )
+from prettytable import PrettyTable
 
 class DisplayStream():
     """
     Methods to format and display Reddit stream objects.
     """
+
+    @staticmethod
+    def populate_table(include_fields, obj, prefix, pretty_stream):
+        """
+        Populate the PrettyTable rows with Reddit object metadata.
+
+        Parameters
+        ----------
+        include_fields: list
+            List containing dictionary keys that will be added to the PrettyTable
+            row
+        obj: dict
+            Dictionary containing Reddit comment or submission data
+        prefix: str
+            String denoting a prefix to prepend to an attribute
+        pretty_stream: PrettyTable instance
+
+        Returns
+        -------
+        None
+        """
+
+        for attribute, data in obj.items():
+            if attribute in include_fields:
+                pretty_stream.add_row([
+                    prefix + attribute,
+                    data
+                ])
 
     @staticmethod
     def display(obj, object_type):
@@ -32,47 +61,61 @@ class DisplayStream():
         None
         """
 
-        if object_type == "submissions":
-            submission_stream = r"""
-created_utc: {created_utc} 
-type: submission
+        pretty_stream = PrettyTable()
+        pretty_stream.field_names = [
+            "%s Attribute" % object_type.capitalize(),
+            "Data"
+        ]
 
-author: {author}
-id: {id}
-is_original_content: {is_original_content}
-is_self: {is_self}
-link_flair_text: {link_flair_text}
-name: {name}
-nsfw: {nsfw}
-num_comments: {num_comments}
-permalink: {permalink}
-score: {score}
-selftext: {selftext}
-spoiler: {spoiler}
-stickied: {stickied}
-title: {title}
-upvote_ratio: {upvote_ratio}
-url: {url}
-""".format(created_utc = obj["created_utc"],
-            author = obj["author"],
-            id = obj["id"],
-            is_original_content = obj["is_original_content"],
-            is_self = obj["is_self"],
-            link_flair_text = obj["link_flair_text"],
-            name = obj["name"],
-            nsfw = obj["nsfw"],
-            num_comments = obj["num_comments"],
-            permalink = obj["permalink"],
-            score = obj["score"],
-            title = obj["title"],
-            selftext = obj["selftext"] \
-                if obj["selftext"] \
-                else None,
-            spoiler = obj["spoiler"],
-            stickied = obj["stickied"],
-            upvote_ratio = obj["upvote_ratio"],
-            url = obj["url"],
-            )
+        if object_type == "submission":
+            include_fields = [
+                "author",
+                "created_utc",
+                "distinguished",
+                "edited",
+                "id",
+                "is_original_content",
+                "is_self",
+                "link_flair_text",
+                "nsfw",
+                "score",
+                "selftext",
+                "spoiler",
+                "stickied",
+                "title",
+                "url"
+            ]
+        elif object_type == "comment":
+            include_fields = [
+                "author",
+                "body",
+                "created_utc",
+                "distinguished",
+                "edited",
+                "id",
+                "is_submitter",
+                "link_id",
+                "parent_id",
+                "score",
+                "stickied"
+            ]
+            
+            submission_fields = [
+                "created_utc",
+                "nsfw",
+                "num_comments",
+                "score",
+                "title",
+                "upvote_ratio",
+                "url",
+            ]
 
-            print(submission_stream)
-            print(Fore.WHITE + Style.BRIGHT + "-" * 30)
+            DisplayStream.populate_table(submission_fields, obj["submission"], "submission_", pretty_stream)
+
+        DisplayStream.populate_table(include_fields, obj, "", pretty_stream)
+
+        pretty_stream.sortby = "%s Attribute" % object_type.capitalize()
+        pretty_stream.align = "l"
+        pretty_stream.max_width = 120
+
+        print(pretty_stream)
