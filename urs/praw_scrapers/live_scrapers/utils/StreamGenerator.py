3e7552c5614b2f821c54e1f2b1758cf5bd1bbd6b
Joseph Lai <jlai24142@gmail.com>
2021-05-14 21:51:02 -0400

    Refactoring comments and submissions streams

--- urs/praw_scrapers/live_scrapers/utils/StreamGenerator.py
+++ urs/praw_scrapers/live_scrapers/utils/StreamGenerator.py
@@ -7,7 +7,6 @@ converts them to JSON serializable objects when saving to file.
 
 
 from urs.praw_scrapers.utils.Objectify import Objectify
-from urs.utils.Global import convert_time
 
 class StreamGenerator():
     """
@@ -16,34 +15,26 @@ class StreamGenerator():
     """
 
     @staticmethod
-    def single_stream(object_type, stream):
+    def stream_submissions(stream):
         """
-        Yield new Reddit objects based on the `object_type` that is passed into 
-        this method.
+        Yield new Reddit submissions.
 
         Parameters
         ----------
-        object_type: str
-            String denoting the Reddit object's type
         stream: Reddit stream instance
 
         Yields
         ------
-        reddit_object: 
+        submission: Reddit submission object 
         """
 
-        if object_type == "submission":
-            for submission in stream.submissions(skip_existing = True):
-                yield Objectify().make_submission(False, submission)
+        for submission in stream.submissions(skip_existing = True):
+            yield Objectify().make_submission(True, submission)
 
-        elif object_type == "comment":
-            for comment in stream.comments(skip_existing = True):
-                yield Objectify().make_comment(comment, True)
-        
     @staticmethod
-    def dual_stream(stream):
+    def stream_comments(stream):
         """
-        Yield both comments and submissions from the stream.
+        Yield new Reddit comments.
 
         Parameters
         ----------
@@ -51,16 +42,9 @@ class StreamGenerator():
 
         Yields
         ------
-        reddit_objects:
+        submission: Reddit comment object 
         """
 
-        for comment, submission in zip(stream.comments(skip_existing = True), stream.submissions(skip_existing = True)):
-            yield {
-                "obj": Objectify().make_submission(False, submission), 
-                "object_type": "submission"
-            }
-
-            yield {
-                "obj": Objectify().make_comment(comment, True), 
-                "object_type": "comment"
-            }
+        for comment in stream.comments(skip_existing = True):
+            yield Objectify().make_comment(comment, True)
+    
