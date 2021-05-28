e9a2a2553a9a4e50b91eec11234801c4f1aefd96
Joseph Lai <jlai24142@gmail.com>
2021-05-28 19:42:11 -0400

    Refactoring Livestream class. Adding logging

--- urs/praw_scrapers/live_scrapers/Livestream.py
+++ urs/praw_scrapers/live_scrapers/Livestream.py
@@ -6,9 +6,10 @@ submissions.
 """
 
 
-import time
 import json
+import logging
 import os
+import time
 
 from colorama import (
     Fore,
@@ -70,8 +71,7 @@ class SaveStream():
 
         Calls public methods from an external module:
 
-            InitializeDirectory.create()
-            InitializeDirectory.make_type_directory()
+            InitializeDirectory.create_dirs()
 
         Parameters
         ----------
@@ -84,15 +84,13 @@ class SaveStream():
             String denoting the path to the directory in which the stream is saved
         """
 
-        InitializeDirectory.make_type_directory("livestream")
-
         if split_stream_info[0] == "r":
             sub_directory = "subreddits"
         elif split_stream_info[0] == "u":
             sub_directory = "redditors"
 
         stream_directory = "../scrapes/%s/livestream/%s" % (date, sub_directory)
-        InitializeDirectory.create(stream_directory)
+        InitializeDirectory.create_dirs(stream_directory)
 
         return stream_directory
 
@@ -121,8 +119,32 @@ class SaveStream():
         filename = split_stream_info[1] + ".json"
 
         stream_directory = SaveStream._make_livestream_dir(split_stream_info)
+        stream_path = stream_directory + "/" + filename
 
-        return stream_directory + "/" + filename
+        logging.info("Writing stream to temporary file: %s." % stream_path)
+
+        return stream_path
+
+    @staticmethod
+    def _create_temp_file(skeleton, stream_path):
+        """
+        Create a temporary file to write stream data in real time.
+
+        Parameters
+        ----------
+        skeleton: dict
+            Dictionary containing data pertaining to the livestream
+        stream_path: str
+            String denoting the path to the saved livestream
+
+        Returns
+        -------
+        None
+        """
+
+        if not os.path.isfile(stream_path):
+            with open(stream_path, "w", encoding = "utf-8") as new_file:
+                json.dump(skeleton, new_file)
 
     @staticmethod
     def _rename(duration, object_info, start_stream, stream_path):
@@ -154,6 +176,8 @@ class SaveStream():
             file_type = split_stream_path[-1]
         )
         
+        logging.info("Renaming livestream file to: %s." % new_filename)
+
         os.rename(stream_path, new_filename)
 
     @staticmethod
@@ -178,26 +202,30 @@ class SaveStream():
         generator: Reddit object generator
         object_info: str
             String denoting which Reddit objects are displayed in the stream
+        stream_info: str
+            String denoting the livestream information
 
         Returns
         -------
-        skeleton: dict
-            Dictionary containing all scrape data
+        stream_statistics: str
+            String denoting the livestream statistics (Reddit objects, Subreddit
+            or Redditor, and duration)
         """
         
         skeleton = SaveStream._create_skeleton(args)
-
         stream_path = SaveStream._get_temp_filename(stream_info)
 
-        if not os.path.isfile(stream_path):
-            with open(stream_path, "w", encoding = "utf-8") as new_file:
-                json.dump(skeleton, new_file)
+        SaveStream._create_temp_file(skeleton, stream_path)
 
         with open(stream_path, "r+", encoding = "utf-8") as existing_file:
             stream_data = json.load(existing_file)
 
             start_stream = time.mktime(time.localtime())
             try:
+                logging.info("")
+                logging.info("STREAMING...")
+                logging.info("")
+
                 for obj in generator:
                     DisplayStream.display(obj)
                     stream_data["data"].append(obj)
@@ -209,10 +237,14 @@ class SaveStream():
             except KeyboardInterrupt:
                 end_stream = time.mktime(time.localtime())
                 duration = time.strftime("%H:%M:%S", time.gmtime(end_stream - start_stream))
+                stream_statistics = "Streamed %s submitted %s for %s." % (object_info, stream_info, duration)
 
                 print("\n\n")
                 Halo().info(Fore.YELLOW + Style.BRIGHT + "ABORTING LIVESTREAM.")
-                Halo().info("Streamed %s submitted %s for %s." % (object_info, stream_info, duration))
+                logging.info("ABORTING LIVESTREAM.")
+                logging.info("")
+
+                Halo().info(stream_statistics)
                 print()
 
                 stream_data["livestream_metadata"]["stream_duration"] = duration
@@ -227,13 +259,61 @@ class SaveStream():
         SaveStream._rename(duration, object_info, time.strftime("%H:%M:%S", time.localtime(start_stream)), stream_path)
         save_spinner.info(Fore.GREEN + Style.BRIGHT + "Livestream has been saved to file.")
 
+        logging.info("Livestream has been saved to file.")
+        logging.info("")
+
         print()
+
+        return stream_statistics
 
 class Livestream():
     """
-    Methods for livestreaming a Subreddit or Redditor's new submissions and/or 
-    comments.
+    Methods for livestreaming a Subreddit or Redditor's new comments or submissions.
     """
+
+    @staticmethod
+    def _set_info_and_object(args, reddit):
+        """
+        Set the stream information and Reddit object based on CLI args.
+
+        Parameters
+        ----------
+        args: Namespace
+            Namespace object containing all arguments that were defined in the CLI
+        reddit: PRAW Reddit object
+
+        Returns
+        -------
+        reddit_object: PRAW Subreddit or Redditor object
+        stream_info: str
+            String denoting the livestream information
+        """
+
+        if args.live_subreddit:
+            PRAWTitles.lr_title()
+
+            Validation.validate([args.live_subreddit], reddit, "subreddit")
+
+            initial_message = "Initializing Subreddit livestream for r/%s." % args.live_subreddit
+            
+            stream_info = "in r/%s" % args.live_subreddit
+            reddit_object = reddit.subreddit(args.live_subreddit)
+
+        elif args.live_redditor:
+            PRAWTitles.lu_title()
+
+            Validation.validate([args.live_redditor], reddit, "redditor")
+
+            initial_message = "Initializing Redditor livestream for u/%s." % args.live_redditor
+            
+            stream_info = "by u/%s" % args.live_redditor
+            reddit_object = reddit.redditor(args.live_redditor)
+        
+        Halo().info(Fore.CYAN + Style.BRIGHT + initial_message)
+        logging.info(initial_message + "..")
+        Halo().info("New entries will appear when posted to Reddit.")
+
+        return reddit_object, stream_info
 
     @staticmethod
     def _stream_switch(args, reddit_object):
@@ -249,7 +329,7 @@ class Livestream():
         Parameters
         ----------
         args: Namespace
-            Namespace object containing all arguments that were defined in the CLI 
+            Namespace object containing all arguments that were defined in the CLI
         reddit_object: Reddit Subreddit or Redditor object
 
         Returns
@@ -271,6 +351,49 @@ class Livestream():
         print()
 
         return generator, object_info
+
+    @staticmethod
+    def _no_save_stream(generator, object_info, stream_info):
+        """
+        Only stream new Reddit comments or submissions. Do not save to file.
+
+        Parameters
+        ----------
+        generator: Reddit object generator
+        object_info: str
+            String denoting which Reddit objects are displayed in the stream
+        stream_info: str
+            String denoting the livestream information
+
+        Returns
+        -------
+        stream_statistics: str
+            String denoting the livestream statistics (Reddit objects, Subreddit
+            or Redditor, and duration)
+        """
+
+        start_stream = time.time()
+
+        try:
+            logging.info("")
+            logging.info("STREAMING...")
+            logging.info("")
+
+            for obj in generator:
+                DisplayStream.display(obj)
+        except KeyboardInterrupt:
+            duration = time.strftime("%H:%M:%S", time.gmtime(time.time() - start_stream))
+            stream_statistics = "Streamed %s submitted %s for %s." % (object_info, stream_info, duration)
+
+            print("\n\n")
+            Halo().info(Fore.YELLOW + Style.BRIGHT + "ABORTING LIVESTREAM.")
+            logging.info("ABORTING LIVESTREAM.")
+            logging.info("")
+
+            Halo().info("Streamed %s submitted %s for %s." % (object_info, stream_info, duration))
+            print()
+
+        return stream_statistics
 
     @staticmethod
     def stream(args, reddit):
@@ -304,42 +427,12 @@ class Livestream():
         None
         """
 
-        if args.live_subreddit:
-            PRAWTitles.lr_title()
-
-            Validation.validate([args.live_subreddit], reddit, "subreddit")
-
-            Halo().info(Fore.CYAN + Style.BRIGHT + "Initializing Subreddit livestream for r/%s." % args.live_subreddit)
-            
-            stream_info = "in r/%s" % args.live_subreddit
-            reddit_object = reddit.subreddit(args.live_subreddit)
-
-        elif args.live_redditor:
-            PRAWTitles.lu_title()
-
-            Validation.validate([args.live_redditor], reddit, "redditor")
-
-            Halo().info(Fore.CYAN + Style.BRIGHT + "Initializing Redditor livestream for u/%s." % args.live_redditor)
-            
-            stream_info = "by u/%s" % args.live_redditor
-            reddit_object = reddit.subreddit(args.live_redditor)
-        
-        Halo().info("New entries will appear when posted to Reddit.")
-
+        reddit_object, stream_info = Livestream._set_info_and_object(args, reddit)
         generator, object_info = Livestream._stream_switch(args, reddit_object)
 
-        if args.nosave:
-            start_stream = time.time()
+        stream_statistics = Livestream._no_save_stream(generator, object_info, stream_info) \
+            if args.nosave \
+            else SaveStream.write(args, generator, object_info, stream_info)
 
-            try:
-                for obj in generator:
-                    DisplayStream.display(obj)
-            except KeyboardInterrupt:
-                duration = time.strftime("%H:%M:%S", time.gmtime(time.time() - start_stream))
-
-                print("\n\n")
-                Halo().info(Fore.YELLOW + Style.BRIGHT + "ABORTING LIVESTREAM.")
-                Halo().info("Streamed %s submitted %s for %s." % (object_info, stream_info, duration))
-                print()
-        else:
-            SaveStream.write(args, generator, object_info, stream_info)
+        logging.info(stream_statistics)
+        logging.info("")
