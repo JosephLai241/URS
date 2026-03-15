cb0d9dc896ae5ce853bde8df84259fd32888157d
Joseph Lai <jlai24142@gmail.com>
2026-03-15 16:39:19 -0500

    Using const fn instead of bare fn

--- crates/urs-core/src/export/json.rs
+++ crates/urs-core/src/export/json.rs
@@ -14,7 +14,7 @@ pub struct JsonExporter;
 impl JsonExporter {
     /// Creates a new JSON exporter.
     #[must_use]
-    pub fn new() -> Self {
+    pub const fn new() -> Self {
         Self
     }
 
